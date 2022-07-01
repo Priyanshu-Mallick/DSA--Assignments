3d5a410c6d183f45a8997531a8bef9687d9b2a4c
Priyanshu Mallick <priyanshumallick123456@gmail.com>
2022-07-01 15:54:30 +0530

    Modified

--- Assignment 11/q9.c
+++ Assignment 11/q9.c
@@ -1,19 +1,9 @@
-/**
- * Add two polynomials
- * Using Linked List
- * @author Swashata
- * @for Dearest Froggie
- */
+// Addition of polynomial using linked list
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * The structure for the polynomial
- * This is a linked list with two variable
- * int coeff The Coefficient
- * int pow The power of x
- */
+
 typedef struct link
 {
     int coeff;
@@ -21,7 +11,7 @@ typedef struct link
     struct link *next;
 } poly;
 
-/** The prototypes */
+
 void createpoly(poly **);
 void showpoly(poly *);
 void addpoly(poly **, poly *, poly *);
@@ -56,45 +46,33 @@ int main(void)
 
 void createpoly(poly **node)
 {
-    int flag; // A flag to control the menu
+    int flag; 
     int coeff, pow;
-    poly *tmp_node;                             // To hold the temporary last address
-    tmp_node = (poly *)malloc(sizeof(poly)); // create the first node
-    *node = tmp_node;                              // Store the head address to the reference variable
+    poly *tmp_node;                             
+    tmp_node = (poly *)malloc(sizeof(poly));
+    *node = tmp_node;                            
     do
     {
-        // Get the user data
         printf("\nEnter Coeff:");
         scanf("%d", &coeff);
         tmp_node->coeff = coeff;
         printf("\nEnter Pow:");
         scanf("%d", &pow);
         tmp_node->pow = pow;
-        // Done storing user data
 
-        // Now increase the Linked on user condition
         tmp_node->next = NULL;
 
-        // Ask user for continuation
         printf("\nContinue adding more terms to the polynomial list?(Y = 1/N = 0): ");
         scanf("%d", &flag);
-        // printf("\nFLAG: %c\n", flag);
-        // Grow the linked list on condition
         if (flag)
         {
-            tmp_node->next = (poly *)malloc(sizeof(poly)); // Grow the list
+            tmp_node->next = (poly *)malloc(sizeof(poly)); 
             tmp_node = tmp_node->next;
             tmp_node->next = NULL;
         }
     } while (flag);
 }
 
-/**
- * The show polynomial function
- * Prints the Polynomial in user readable format
- * @param poly * node The polynomial linked list
- * @return void
- */
 void showpoly(poly *node)
 {
     printf("\nThe polynomial expression is:\n");
@@ -107,22 +85,14 @@ void showpoly(poly *node)
     }
 }
 
-/**
- * The polynomial add function
- * Adds two polynomial to a given variable
- * @param poly ** result Stores the result
- * @param poly * poly1 The first polynomial expression
- * @param poly * poly2 The second polynomial expression
- * @return void
- */
+
 void addpoly(poly **result, poly *poly1, poly *poly2)
 {
-    poly *tmp_node; // Temporary storage for the linked list
+    poly *tmp_node; 
     tmp_node = (poly *)malloc(sizeof(poly));
     tmp_node->next = NULL;
-    *result = tmp_node; // Copy the head address to the result linked list
+    *result = tmp_node;
 
-    // Loop while both of the linked lists have value
     while (poly1 && poly2)
     {
         if (poly1->pow > poly2->pow)
@@ -145,7 +115,6 @@ void addpoly(poly **result, poly *poly1, poly *poly2)
             poly2 = poly2->next;
         }
 
-        // Grow the linked list on condition
         if (poly1 && poly2)
         {
             tmp_node->next = (poly *)malloc(sizeof(poly));
@@ -154,11 +123,8 @@ void addpoly(poly **result, poly *poly1, poly *poly2)
         }
     }
 
-    // Loop while either of the linked lists has value
     while (poly1 || poly2)
     {
-        // We have to create the list at beginning
-        // As the last while loop will not create any unnecessary node
         tmp_node->next = (poly *)malloc(sizeof(poly));
         tmp_node = tmp_node->next;
         tmp_node->next = NULL;
